093d60632e351a0376a16457537d450911874445
marct <marc.takeuchi@gmail.com>
2019-06-28 17:58:59 -0700

    ^updating copy

--- includes/efelle-1.php
+++ includes/efelle-1.php
@@ -4,14 +4,16 @@
 <p><strong>Responsibilities:</strong></p>
 
 <ul>
+	<li>Collaborate with clients in creating new sitemap and information architecture strategy</li>
+	<li>Recommend improved user experience (UX) solutions</li>
 	<li>Work closely with the sales and design teams to ensure project specifications and scope are met</li>
 	<li>Assist the sales and account management teams in developing solutions to technical issues</li>
 	<li>Train and mentor Account Managers and interns in content entry and formatting in the CMS along with work-flow processes and unit testing</li>
 	<li>Setup repository, development and production server and database environments</li>
 	<li>Manage DNS, SSL installation and email configuration (G-Suite)</li>
 	<li>Document internal process and procedures</li>  
-	<li>Conduct one-on-one CMS training for our clients</li>
-	<li>Provide technical on-going support for clients</li>
+	<li>Conduct onsite CMS training for all clients</li>
+	<li>Provide technical support</li>
 </ul>
 
 <p><strong>Technology used:</strong><br />
--- includes/skills.php
+++ includes/skills.php
@@ -2,7 +2,7 @@
 <p>People!, Web UX/UI Design, Web Usability, Information Architecture, Systems Analysis, SEO, Tacos, Learning!</p>
 
 <p><strong>Tools:</strong></p>
-<p>Adobe CC, Sublime Text, GitHub, SQL Pro, Heidi SQL, G-Suite for Business, Basecamp, Trello, Slack, Paymo, Office 365, BigCommerce</p>
+<p>Adobe CC, Sublime Text, GitHub, SQL Pro, Heidi SQL, G-Suite for Business, Basecamp, Slickplan, Trello, Slack, Paymo, Office 365, BigCommerce</p>
 
 <p><strong>Code:</strong></p>
 <p>HTML5, CSS3, Sass, Bootstrap, jQuery, Javascript, SQL, PHP, ColdFusion</p>
